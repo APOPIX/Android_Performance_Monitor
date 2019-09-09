675115a6599df75ff7c112ed831f2158d4871b88
zanhanding <zanhanding@tencent.com>
2019-09-09 14:44:55 +0800

    加入PSS监控功能、增强兼容性

--- run.py
+++ run.py
@@ -1,58 +1,86 @@
 # encoding:utf-8
-import os
-import time
-
 import logging
+import os
 
 
-def get_app_cpu_usage(package_name, device_name="", device_cores=-1, silence=True):
-    # type: (str, str,int,bool) -> float
+def get_app_cpu_usage(package_name, device_name="", device_cores=-1):
+    # type: (str, str,int) -> float
     """
     :param package_name: 待检测安卓应用包名【必填】
     :param device_name: 待检测设备名（使用“adb devices”命令获取）【若为空则检测默认设备，当设备数大于一时将报错】
-    :param device_cores: 设备CPU核心数，为-1时程序将自动获取核心数（依赖adb工具的输出格式，不保证后续版本的正确性）
-    :param silence: 是否静默运行，为True时只打印错误信息，为False时将会打印全部信息
-    :return: 该设备指定应用的CPU占用率（例如：占用15%则return 0.15）
-
-    使用前，请确保您已正确安装adb工具并将其添加到系统环境变量，若您使用pycharm来执行此方法，
-    请确认pycharm的环境配置是否包括了adb所在路径，即pycharm下运行shell命令是否能够调用adb工具
-
-    验证方法：
-    在pycharm的terminal中输入adb，是否能够执行相关命令，若显示“'adb' 不是内部或外部命令，也不是可运行的程序”
-    请在File->Settings->Tools->Terminal->Project Settings->Environment Variables中确认是否包含了adb的环境变量
+    :param device_cores: 设备核心数，从adb获取到的使用率可能会超过100%，指定这个数字来修正获得的结果（旧版安卓系统指定为1即可）
+    :return: 该设备指定应用的CPU占用率（占用15%则return 0.15）
     """
     if device_name.__len__() > 0:
-        device_name = "-s" + device_name
+        device_name = "-s " + device_name
     else:
-        if not silence:
-            print "未指定设备名，尝试连接默认设备"
-    if device_cores is -1 and not silence:
+        print "未指定设备名，尝试连接默认设备"
+    if device_cores is -1:
         print "未指定CPU核心数，尝试自动获取（结果不一定可靠）"
         try:
             cmd = 'adb ' + device_name + ' shell top -n 1'
+            # print cmd
             device_cores = int(os.popen(cmd).readlines()[3].split('%cpu')[0]) / 100
         except:
-            logging.error("获取CPU核心数失败")
+            device_cores = 1
+            logging.error("获取CPU核心数失败，请手动指定CPU核心数！")
     try:
         # 获取进程ID
         cmd = 'adb ' + device_name + ' shell ps -o PID,NAME | findstr ' + package_name
+        # print cmd
         p = os.popen(cmd)
         PID = p.readline().split()[0]
     except:
-        logging.error("获取进程ID失败")
+        try:
+            # 获取进程ID（旧版命令）
+            cmd = 'adb ' + device_name + ' shell ps | findstr ' + package_name
+            # print cmd
+            p = os.popen(cmd)
+            PID = p.readline().split()[1]
+        except:
+            logging.error("获取进程ID失败，请确认包名是否正确、程序是否打开")
     try:
         # 根据进程ID获取CPU使用率
         cmd = 'adb ' + device_name + ' shell top -n 1  -o %CPU,PID  | findstr ' + PID
+        # print cmd
         p = os.popen(cmd)
         return float(p.readline().split()[0]) / (device_cores * 100)
     except:
-        logging.error("获取CPU使用率失败")
+        try:
+            # 根据进程ID获取CPU使用率（旧版命令）
+            cmd = 'adb ' + device_name + ' shell top -n 1 | findstr ' + PID
+            # print cmd
+            p = os.popen(cmd)
+            output = p.readline().split()
+            for i in range(0, output.__len__()):
+                if output[i].__contains__('%'):
+                    return float(output[i].split('%')[0]) / (device_cores * 100)
+            #     若数据中不带%，尝试直接读取第八列
+            return float(output[8]) / (device_cores * 100)
+        except:
+            logging.error("获取CPU使用率失败")
     return -1
 
 
+def get_app_pss_in_KB(package_name, device_name=""):
+    if device_name.__len__() > 0:
+        device_name = "-s " + device_name
+    else:
+        print "未指定设备名，尝试连接默认设备"
+    try:
+        cmd = 'adb ' + device_name + ' shell dumpsys meminfo -package | findstr ' + package_name
+        # print cmd
+        output = os.popen(cmd).readlines()
+        return int(output[0].replace(' ', '').replace('k', 'K').split('K')[0].replace(',', ''))
+    except:
+        logging.error("获取PSS失败，请尝试更改输出信息提取规则，控制台输出为：" + output)
+
+
 if __name__ == "__main__":
-    # 请指定待测程序包名
-    package_name = 'com.tencent.liteav.demo'
     while True:
-        print "当前应用的CPU用量：%f%%" % (get_app_cpu_usage(package_name, silence=False) * 100)
-        time.sleep(2)  # 每隔2秒获取一次
+        print "current app's cpu usage: %f%%" % (
+                get_app_cpu_usage("com.tencent.liteav.demo", device_name='80QBCNQ22W8F', device_cores=1) * 100)
+        print "current app's pss: %d KB" % (
+            get_app_pss_in_KB(package_name='com.tencent.liteav.demo', device_name='80QBCNQ22W8F'))
+
+        # time.sleep(2)  # 每隔2秒获取一次
